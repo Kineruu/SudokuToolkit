e8f74216903d9cdd00c0ad86b13b61c6240e5dd4
Kineruu <Kineru08@gmail.com>
2026-03-30 01:33:13 +0200

    Fixed output once again

--- generate.py
+++ generate.py
@@ -44,12 +44,32 @@ def getPossibleRowNumbers():
 
         possibleRowNumbers.append(numbers)
 
-def printBoard():
-    for i in range(9):
-        print(possibleRowNumbers[i])
+#def printBoard():
+#    for i in range(9):
+#        print(possibleRowNumbers[i])
+#
+#       if (i + 1) % 3 == 0:
+#            print()
 
-        if (i + 1) % 3 == 0:
-            print()
+# Printing out the board...?
+def printBoard():
+    # For each row that's in the userNumbers
+    for i in range(9):
+        # Every 3 rows and if i is not equal to 0 so it doesn't write it at the beginning
+        if i % 3 == 0 and i != 0:
+            # Prints it "*" 21 times
+            print("-" * 21)
+    
+        for j in range(9):
+            # Every 3 numbers
+            if j % 3 == 0 and j != 0:
+                print("|", end=" ")
+
+            number = possibleRowNumbers[i][j]
+            value = number if number != 0 else "_"
+            print(value, end=" ")
+    
+        print()
 
 if __name__ == "__main__":
     getPossibleRowNumbers()
